30a0eaf06a53ee1893fc41866b1250e750405809
MPKaronde <karonde.manav@gmail.com>
2024-12-19 12:38:12 -0600

    found correct pitch and yaw degree conversion constants

--- StarTracker/src/runner.cpp
+++ StarTracker/src/runner.cpp
@@ -5,12 +5,12 @@
 #define HALFSTEP 4
 
 // (1 / 1.8) Steps per degree for motor * gear ratio
-double YAW_STEPS_PER_DEGREE = (1 / 1.8) * (30 / 120); 
-double PITCH_STEPS_PER_DEGREE = (1 / 1.8) * (30 / 110);
+double YAW_STEPS_PER_DEGREE = 2.2;   //(1 / 1.8) * (30 / 120);
+double PITCH_STEPS_PER_DEGREE = 2.1; //(1 / 1.8) * (30 / 110);
 
 // Hardware
-AccelStepper pitchMotor(HALFSTEP, 2, 3, 4, 5);
-AccelStepper yawMotor(HALFSTEP, 6, 7, 8, 9);
+AccelStepper pitchMotor(HALFSTEP, 8, 9, 10, 11);
+AccelStepper yawMotor(HALFSTEP, 2, 3, 4, 5);
 QMC5883LCompass compass;
 
 // Motor Constants
@@ -53,11 +53,11 @@ boolean rotatePitchByDegrees(double deg)
     pitchMotor.setCurrentPosition(0);
     pitchMotor.moveTo(deg * PITCH_STEPS_PER_DEGREE);
 
-    while(pitchMotor.distanceToGo() != 0)
+    while (pitchMotor.distanceToGo() != 0)
     {
         pitchMotor.run();
     }
-    
+
     currentPitch += deg;
     return true;
 }
@@ -69,7 +69,7 @@ boolean rotateYawByDegrees(double deg)
     yawMotor.setCurrentPosition(0);
     yawMotor.moveTo(deg * YAW_STEPS_PER_DEGREE);
 
-    while(yawMotor.distanceToGo() != 0)
+    while (yawMotor.distanceToGo() != 0)
     {
         yawMotor.run();
     }
@@ -92,13 +92,13 @@ boolean pointInDirection(double targetAzimuth, double targetPitch)
 
     // move to appropriate yaw
     int currentAzimuth = getAzimuth();
-    if(!rotateYawByDegrees(targetAzimuth - currentAzimuth))
+    if (!rotateYawByDegrees(targetAzimuth - currentAzimuth))
     {
         done = false;
     }
 
     // move to appropriate pitch
-    if(!rotatePitchByDegrees(targetPitch - currentPitch))
+    if (!rotatePitchByDegrees(targetPitch - currentPitch))
     {
         done = false;
     }
@@ -106,9 +106,12 @@ boolean pointInDirection(double targetAzimuth, double targetPitch)
     return done;
 }
 
-
-
-
-
-
-
+boolean done = false;
+void loop()
+{
+    if (!done)
+    {
+        rotatePitchByDegrees(180);
+        done = true;
+    }
+}
